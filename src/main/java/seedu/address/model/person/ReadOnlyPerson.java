ae9c469beb9e16313a6079e600e8549dfa5fe92e
Karrui Lau <karrui.lau@gmail.com>
2017-10-03 17:13:23 +0800

    Model::Person: Revert ReadOnlyPerson equals method
    It was causing duplicate entries to be accepted when no homepage was
    specified, as it would be matching between the default homepage and
    no homepage.

--- src/main/java/seedu/address/model/person/ReadOnlyPerson.java
+++ src/main/java/seedu/address/model/person/ReadOnlyPerson.java
@@ -34,8 +34,7 @@ public interface ReadOnlyPerson {
                 && other.getName().equals(this.getName()) // state checks here onwards
                 && other.getPhone().equals(this.getPhone())
                 && other.getEmail().equals(this.getEmail())
-                && other.getAddress().equals(this.getAddress()))
-                && other.getHomepage().equals(this.getHomepage());
+                && other.getAddress().equals(this.getAddress()));
     }
 
     /**
