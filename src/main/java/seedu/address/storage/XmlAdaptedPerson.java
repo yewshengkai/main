dd805ca5a2fd4458aee494da7c65e7e36ac04dee
Karrui Lau <karrui.lau@gmail.com>
2017-10-03 17:46:17 +0800

    Storage::XmlAdaptedPerson: Add homepage to be saved/loaded from storage
    WARNING: Not backwards compatible -- if addressbook did not have any
    prior homepages, there will be exceptions thrown and the application
    will stop working.
    
    TO FIX: Either delete the entire addressbook, or manually add in
    <homepage>{the correct URL}</homepage> manually.
    
    TO DO: Will figure out a way to make it backwards compatible.

--- src/main/java/seedu/address/storage/XmlAdaptedPerson.java
+++ src/main/java/seedu/address/storage/XmlAdaptedPerson.java
@@ -10,6 +10,7 @@ import javax.xml.bind.annotation.XmlElement;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.Homepage;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
@@ -29,6 +30,8 @@ public class XmlAdaptedPerson {
     private String email;
     @XmlElement(required = true)
     private String address;
+    @XmlElement(required = true)
+    private String homepage;
 
     @XmlElement
     private List<XmlAdaptedTag> tagged = new ArrayList<>();
@@ -50,6 +53,7 @@ public class XmlAdaptedPerson {
         phone = source.getPhone().value;
         email = source.getEmail().value;
         address = source.getAddress().value;
+        homepage = source.getHomepage().value;
         tagged = new ArrayList<>();
         for (Tag tag : source.getTags()) {
             tagged.add(new XmlAdaptedTag(tag));
@@ -71,6 +75,7 @@ public class XmlAdaptedPerson {
         final Email email = new Email(this.email);
         final Address address = new Address(this.address);
         final Set<Tag> tags = new HashSet<>(personTags);
-        return new Person(name, phone, email, address, tags);
+        final Homepage homepage = new Homepage(this.homepage);
+        return new Person(name, phone, email, address, tags, homepage);
     }
 }
