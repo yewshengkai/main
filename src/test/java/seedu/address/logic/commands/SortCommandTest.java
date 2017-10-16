bc9570c5f023dbe04740aa9956710b1bb95adb3f
Karrui Lau <karrui.lau@gmail.com>
2017-10-16 15:34:37 +0800

    Test::Logic:SortCommand: Add new test cases

---
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -0,0 +1,54 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+import static seedu.address.testutil.TypicalPersons.getTypicalDescendingOrderAddressBook;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for SortCommand.
+ */
+public class SortCommandTest {
+
+    private static final boolean DESCENDING = true;
+    private static final boolean ASCENDING = false;
+
+    private Model model;
+
+    @Test
+    public void execute_decendingSort_success() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        SortCommand sortCommand = prepareCommand(DESCENDING);
+        Model expectedModel = new ModelManager(getTypicalDescendingOrderAddressBook(), new UserPrefs());
+
+        String expectedMessage = SortCommand.MESSAGE_SUCCESS;
+
+        assertCommandSuccess(sortCommand, model, expectedMessage, expectedModel);
+
+    }
+
+    @Test
+    public void execute_ascendingSort_success() {
+        model = new ModelManager(getTypicalDescendingOrderAddressBook(), new UserPrefs());
+        SortCommand sortCommand = prepareCommand(ASCENDING);
+        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+        String expectedMessage = SortCommand.MESSAGE_SUCCESS;
+
+        assertCommandSuccess(sortCommand, model, expectedMessage, expectedModel);
+    }
+
+    private SortCommand prepareCommand(boolean isDescendingSort) {
+        SortCommand sortCommand = new SortCommand(isDescendingSort);
+        sortCommand.setData(model, new CommandHistory(), new UndoRedoStack());
+        return sortCommand;
+    }
+}
