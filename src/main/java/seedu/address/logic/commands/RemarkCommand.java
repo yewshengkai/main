0e2624778f9804cf3dc878f797e3650108875025
Karrui Lau <karrui.lau@gmail.com>
2017-10-24 02:38:30 +0800

    Logic::SetAvatarCommand: Add logic for SetAvatarCommand

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -61,7 +61,7 @@ public class RemarkCommand extends UndoableCommand {
 
         ReadOnlyPerson personToEdit = lastShownList.get(index.getZeroBased());
         Person editedPerson = new Person(personToEdit.getName(), personToEdit.getPhone(), personToEdit.getEmail(),
-                personToEdit.getAddress(), remark, personToEdit.getTags());
+                personToEdit.getAddress(), remark, personToEdit.getAvatar(), personToEdit.getTags());
 
         try {
             model.updatePerson(personToEdit, editedPerson);
