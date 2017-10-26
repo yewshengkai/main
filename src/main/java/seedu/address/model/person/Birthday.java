993b42a6326dcb50a930cd84b072642a35c82a13
basyiro <bashbashbasyir@hotmail.com>
2017-10-26 22:58:06 +0800

    Model: added Birthday class

---
+++ src/main/java/seedu/address/model/person/Birthday.java
@@ -0,0 +1,44 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+
+public class Birthday {
+
+    public static final String MESSAGE_BIRTHDAY_CONSTRAINTS =
+            "Birthday should only contain numbers, separated by '/'";
+    public static final String BIRTHDAY_VALIDATION_REGEX = "[\\d\\.]+/[\\d\\.]+/[\\d\\.]+";
+
+    public final String value;
+
+    public Birthday(String birthday) throws IllegalValueException {
+        requireNonNull(birthday);
+        String trimmedBirthday = birthday.trim();
+        if (!isValidBirthday(trimmedBirthday)) {
+            throw new IllegalValueException(MESSAGE_BIRTHDAY_CONSTRAINTS);
+        }
+        this.value = trimmedBirthday;
+    }
+
+    public static boolean isValidBirthday(String test) {
+        return test.matches(BIRTHDAY_VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Email // instanceof handles nulls
+                && this.value.equals(((Email) other).value)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
