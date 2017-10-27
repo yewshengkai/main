bb4be28b7405ddcc0793bfc42731fd165d7200a7
Karrui Lau <karrui.lau@gmail.com>
2017-10-27 15:07:34 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
+++ src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
@@ -6,7 +6,6 @@ import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_HOMEPAGE_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_HOMEPAGE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
@@ -57,8 +56,10 @@ public class EditPersonDescriptorTest {
         editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withTags(VALID_TAG_HUSBAND).build();
         assertFalse(DESC_AMY.equals(editedAmy));
 
+        //@@author karrui
         // different homepage -> returns false
         editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withHomepage(VALID_HOMEPAGE_BOB).build();
         assertFalse(DESC_AMY.equals(editedAmy));
+        //@@author
     }
 }
