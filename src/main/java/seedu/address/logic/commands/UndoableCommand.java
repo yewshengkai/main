0ff175ca8d94564825bf6fd50c0b9cc6a886bc29
Karrui Lau <karrui.lau@gmail.com>
2017-11-01 10:39:29 +0800

    Logic::Commands: Fix undo-redo-undo chain bug

--- src/main/java/seedu/address/logic/commands/UndoableCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoableCommand.java
@@ -49,6 +49,7 @@ public abstract class UndoableCommand extends Command {
     protected final void redo() {
         requireNonNull(model);
         try {
+            saveAddressBookSnapshot();
             executeUndoableCommand();
         } catch (CommandException ce) {
             throw new AssertionError("The command has been successfully executed previously; "
