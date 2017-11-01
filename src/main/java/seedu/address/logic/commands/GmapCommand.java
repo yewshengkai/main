20afbe771912ebbef90d2f00add2117e9dba0ec9
Yew Sheng Kai <yew_sheng_kai@hotmail.com>
2017-11-02 04:20:40 +0800

    Logic: Modify GmapCommand to utilize MapToListRequest event handler

--- src/main/java/seedu/address/logic/commands/GmapCommand.java
+++ src/main/java/seedu/address/logic/commands/GmapCommand.java
@@ -1,11 +1,13 @@
 package seedu.address.logic.commands;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
-import seedu.address.commons.events.ui.JumpToListRequestEvent;
+import seedu.address.commons.events.ui.MapToListRequestEvent;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.person.ReadOnlyPerson;
 
@@ -23,9 +25,11 @@ public class GmapCommand extends Command {
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
-    public static final String MESSAGE_SELECT_PERSON_SUCCESS = "Selected Person: %1$s";
+    public static final String MESSAGE_SELECT_PERSON_SUCCESS = "Mapping Person address: %1$s";
 
     private final Index targetIndex;
+
+    private final Logger logger = LogsCenter.getLogger(this.getClass());
 
     public GmapCommand(Index targetIndex) {
         this.targetIndex = targetIndex;
@@ -40,7 +44,7 @@ public class GmapCommand extends Command {
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
 
-        EventsCenter.getInstance().post(new JumpToListRequestEvent(targetIndex));
+        EventsCenter.getInstance().post(new MapToListRequestEvent(lastShownList.get(targetIndex.getZeroBased())));
         return new CommandResult(String.format(MESSAGE_SELECT_PERSON_SUCCESS, targetIndex.getOneBased()));
 
     }
