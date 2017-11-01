6963ebf7d61d402407ccc5b5f07ab81967ab55de
Yew Sheng Kai <yew_sheng_kai@hotmail.com>
2017-11-02 02:49:46 +0800

    Logic: Add GmapCommand

---
+++ src/main/java/seedu/address/logic/commands/GmapCommand.java
@@ -0,0 +1,54 @@
+package seedu.address.logic.commands;
+
+import java.util.List;
+
+import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.events.ui.JumpToListRequestEvent;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.person.ReadOnlyPerson;
+
+/**
+ * Selects a person identified using it's last displayed index from the address book.
+ */
+public class GmapCommand extends Command {
+
+    public static final String COMMAND_WORD = "gmap";
+    public static final String COMMAND_ALIAS = "gm";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Selects and map the person adress to google map identified by the index number used in the last "
+            + "person listing.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_SELECT_PERSON_SUCCESS = "Selected Person: %1$s";
+
+    private final Index targetIndex;
+
+    public GmapCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute() throws CommandException {
+
+        List<ReadOnlyPerson> lastShownList = model.getFilteredPersonList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        EventsCenter.getInstance().post(new JumpToListRequestEvent(targetIndex));
+        return new CommandResult(String.format(MESSAGE_SELECT_PERSON_SUCCESS, targetIndex.getOneBased()));
+
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof GmapCommand // instanceof handles nulls
+                && this.targetIndex.equals(((GmapCommand) other).targetIndex)); // state check
+    }
+}
