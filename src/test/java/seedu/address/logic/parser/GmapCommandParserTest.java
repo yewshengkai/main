b72e028bd1432563a42b007b5ff4d3b2b2875380
Yew Sheng Kai <yew_sheng_kai@hotmail.com>
2017-11-02 17:00:54 +0800

    Logic: Add GmapCommandTest codes

--- src/test/java/seedu/address/logic/parser/GmapCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/GmapCommandParserTest.java
@@ -1,5 +1,29 @@
-import static org.junit.Assert.*;
+package seedu.address.logic.parser;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.GmapCommand;
+
+/**
+ * Test scope: similar to {@code DeleteCommandParserTest}.
+ * @see DeleteCommandParserTest
+ */
 public class GmapCommandParserTest {
 
+    private GmapCommandParser parser = new GmapCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsSelectCommand() {
+        assertParseSuccess(parser, "1", new GmapCommand(INDEX_FIRST_PERSON));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, GmapCommand.MESSAGE_USAGE));
+    }
 }
