7acbce6b297cb3b9e1e93ee6d794cd0d8da6db4b
Yew Sheng Kai <yew_sheng_kai@hotmail.com>
2017-11-09 13:29:48 +0800

    Add code to remove all person with Tag

--- src/main/java/seedu/address/logic/commands/RemoveTag.java
+++ src/main/java/seedu/address/logic/commands/RemoveTag.java
@@ -1,4 +1,55 @@
 package seedu.address.logic.commands;
 
-public class RemoveTag {
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.person.exceptions.DuplicatePersonException;
+import seedu.address.model.person.exceptions.PersonNotFoundException;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Remove tag of all person in the address book.
+ */
+public class RemoveTag extends UndoableCommand {
+
+    public static final String COMMAND_WORD = "removetag";
+    public static final String COMMAND_ALIAS = "rt";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Remove tag of all person in the address book. "
+            + "Parameters: "
+            + PREFIX_TAG + "friends ";
+
+    public static final String MESSAGE_SUCCESS = "All person with specified tag removed: %1$s";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
+    public static final String MESSAGE_PERSON_TAG_NOT_FOUND = "Person with specified tag does not exist in addressbook.";
+
+    private Tag tags;
+
+    /**
+     * Creates an RemoveTag to remove the specified {@code tag}
+     */
+    public RemoveTag(Tag tags) {
+        this.tags = tags;
+    }
+
+    @Override
+    public CommandResult executeUndoableCommand() throws CommandException {
+        requireNonNull(model);
+        try {
+            model.removeTag(tags);
+            return new CommandResult(String.format(MESSAGE_SUCCESS, tags));
+        } catch (DuplicatePersonException e) {
+            throw new CommandException(MESSAGE_DUPLICATE_PERSON);
+        } catch (PersonNotFoundException e) {
+            throw new CommandException(MESSAGE_PERSON_TAG_NOT_FOUND);
+        }
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof RemoveTag // instanceof handles nulls
+                && tags.equals(((RemoveTag) other).tags));
+    }
 }
