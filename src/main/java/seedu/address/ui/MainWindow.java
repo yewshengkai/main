9f79f02a06bee1fffc149f4facd0e4630919b66f
Yew Sheng Kai <yew_sheng_kai@hotmail.com>
2017-11-09 21:56:10 +0800

    Remove unused future enhancement feature

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -1,7 +1,5 @@
 package seedu.address.ui;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.util.logging.Logger;
 
 import com.google.common.eventbus.Subscribe;
@@ -15,7 +13,6 @@ import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import seedu.address.MainApp;
 import seedu.address.commons.core.Config;
@@ -164,7 +161,6 @@ public class MainWindow extends UiPart<Region> {
         CommandBox commandBox = new CommandBox(logic);
         commandBoxPlaceholder.getChildren().add(commandBox.getRoot());
     }
-    //@@author
 
     void hide() {
         primaryStage.hide();
@@ -268,54 +264,6 @@ public class MainWindow extends UiPart<Region> {
     private void handleExit() {
         raise(new ExitAppRequestEvent());
     }
-
-    //@@author yewshengkai-reused
-    /**
-     * Open a file
-     */
-    @FXML
-    private void handleImport() {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Import from...");
-        fileChooser.showOpenDialog(primaryStage);
-    }
-
-    /**
-     * Save file to specific format
-     */
-    @FXML
-    private void handleExport() {
-        FileChooser fileChooser = new FileChooser();
-
-        FileChooser.ExtensionFilter excelFilter = new FileChooser.ExtensionFilter("Excel Workbook (*.xlsx)", "*.xlsx");
-        FileChooser.ExtensionFilter pdfFilter = new FileChooser.ExtensionFilter("PDF (*.pdf)", "*.pdf");
-        FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("XML Data (*.xml)", "*.xml");
-        FileChooser.ExtensionFilter textFilter = new FileChooser.ExtensionFilter("Text Documents (*.txt)", "*.txt");
-
-        fileChooser.getExtensionFilters().add(excelFilter);
-        fileChooser.getExtensionFilters().add(pdfFilter);
-        fileChooser.getExtensionFilters().add(xmlFilter);
-        fileChooser.getExtensionFilters().add(textFilter);
-
-        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
-        fileChooser.setTitle("Export to..");
-        fileChooser.setInitialFileName("iungoAB");
-        File file = fileChooser.showSaveDialog(primaryStage);
-
-        if (file != null) {
-            String content = "Prep_data_save_to_excel";
-            try {
-                FileWriter fileWriter = null;
-                fileWriter = new FileWriter(file);
-                fileWriter.write(content);
-                fileWriter.close();
-            } catch (Exception ex) {
-                System.out.println(ex.toString());
-            }
-
-        }
-    }
-    //@@author
 
     public PersonListPanel getPersonListPanel() {
         return this.personListPanel;
