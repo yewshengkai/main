499ed71d8250a4948b6f8d4f5285d4d8d8c335f7
Yew Sheng Kai <yew_sheng_kai@hotmail.com>
2017-11-09 19:47:42 +0800

    Checkstyle update

--- src/main/java/seedu/address/logic/parser/SelectCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SelectCommandParser.java
@@ -2,9 +2,7 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.core.index.Index;
-import seedu.address.commons.events.ui.PersonSideCardRequestEvent;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.SelectCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
