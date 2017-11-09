3a342fd7e078d6ab6a097ffd21c54f88b92f54c2
Yew Sheng Kai <yew_sheng_kai@hotmail.com>
2017-11-09 14:01:13 +0800

    Add RemoveTagTest and ParserTest skeleton

--- src/test/java/seedu/address/logic/commands/RemoveTagTest.java
+++ src/test/java/seedu/address/logic/commands/RemoveTagTest.java
@@ -1,5 +1,16 @@
+package seedu.address.logic.commands;
+
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class RemoveTagTest {
+    @Test
+    public void executeUndoableCommand() throws Exception {
+    }
+
+    @Test
+    public void equals() throws Exception {
+    }
 
 }
--- src/test/java/seedu/address/logic/parser/RemoveTagParserTest.java
+++ src/test/java/seedu/address/logic/parser/RemoveTagParserTest.java
@@ -1,5 +1,12 @@
+package seedu.address.logic.parser;
+
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class RemoveTagParserTest {
+    @Test
+    public void parse() throws Exception {
+    }
 
 }
