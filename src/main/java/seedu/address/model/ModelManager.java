1236bf2c747c5b59db16e10c08756d266abe9649
Sheng Kai <31110233+yewshengkai@users.noreply.github.com>
2017-11-09 13:07:01 +0800

    checkstyle update

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -3,6 +3,7 @@ package seedu.address.model;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.logging.Logger;
