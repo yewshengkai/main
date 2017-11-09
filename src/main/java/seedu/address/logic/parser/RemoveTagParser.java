b64f976206b04a9e59f8dd75c1720c7ada4fba21
Yew Sheng Kai <yew_sheng_kai@hotmail.com>
2017-11-09 13:59:55 +0800

    Add code RemoveTagParser to accept tag values

--- src/main/java/seedu/address/logic/parser/RemoveTagParser.java
+++ src/main/java/seedu/address/logic/parser/RemoveTagParser.java
@@ -1,4 +1,34 @@
 package seedu.address.logic.parser;
 
-public class RemoveTagParser {
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NONE;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.RemoveTag;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.tag.Tag;
+
+//@@author yewshengkai
+/**
+ * Parses input arguments and creates a new RemoveTag object
+ */
+public class RemoveTagParser implements Parser<RemoveTag> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the RemoveTag
+     * and returns an RemoveTag object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public RemoveTag parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NONE);
+        try {
+            Tag tag = ParserUtil.parseTag(argMultimap.getValue(PREFIX_NONE)).get();
+            return new RemoveTag(tag);
+        } catch (IllegalValueException ive) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemoveTag.MESSAGE_USAGE));
+        }
+    }
+
 }
