900bb8d42d0aace1a39616105aa6570a79b86f9e
Yew Sheng Kai <yew_sheng_kai@hotmail.com>
2017-11-11 18:16:53 +0800

    Update ReadOnlyPerson.java to allow PersonSideCard panel to reflect changes on avatar when edit command is executed

--- src/main/java/seedu/address/model/person/ReadOnlyPerson.java
+++ src/main/java/seedu/address/model/person/ReadOnlyPerson.java
@@ -41,6 +41,7 @@ public interface ReadOnlyPerson {
                 && other.getEmail().equals(this.getEmail())
                 && other.getAddress().equals(this.getAddress()))
                 && other.getHomepage().equals(this.getHomepage())
+                && other.getAvatar().equals(this.getAvatar())
                 && other.getRemark().equals(this.getRemark());
     }
 
